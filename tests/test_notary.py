470e5e0ac7b4968fb19eccf0ecd48c9d7c8f90a6
Tyler Florez <qualiabyte@gmail.com>
2014-09-30 22:50:45 -0700

    Add notary contract

---
+++ tests/test_notary.py
@@ -0,0 +1,50 @@
+from helpers import *
+
+#
+# NOTARY
+#
+
+def test_notary_lll():
+    path = 'contracts/notary.lll'
+    dbpath = 'contracts/objectdb.lll'
+    state = tester.state()
+    db = lll(state, dbpath)
+    contract = lll(state, path)
+    notary_tests(state, contract, db)
+
+
+def notary_tests(s, c, db):
+
+    # Set database address
+    assert s.send(t.k0, c, 0, [z('database'), db]) == []
+    assert s.block.get_storage_data(c, 1) == int(db, 16)
+
+    # Define first account as submitter
+    submitter = int(t.a0, 16)
+
+    # Create a 20-byte (160-bit) hash
+    hash_hex = '0123456789' * 4
+    hash = hash_hex.decode('hex')
+
+    # Notarise a hash, verify the account and timestamp
+    assert s.send(t.k0, c, 0, [z('record'), hash]) == []
+    assert s.send(t.k0, c, 0, [z('get'), hash, "account"]) == [submitter]
+    assert s.send(t.k0, c, 0, [z('get'), hash, "timestamp"]) == [s.block.timestamp]
+
+    # Set a custom property
+    assert s.send(t.k0, c, 0, [z('set'), hash, "author", "Alice"]) == []
+    assert s.send(t.k0, c, 0, [z('get'), hash, "author"]) == [str2num("Alice")]
+
+    # Setting by non-submitters should fail
+    assert s.send(t.k1, c, 0, [z('set'), hash, "author2", "Eve"]) == [1]
+    assert s.send(t.k0, c, 0, [z('get'), hash, "author2"]) == [0]
+
+    # Setting reserved keys should fail
+    assert s.send(t.k0, c, 0, [z('set'), hash, "account", 0xff]) == [2]
+    assert s.send(t.k0, c, 0, [z('set'), hash, "timestamp", 1234]) == [2]
+    assert s.send(t.k0, c, 0, [z('get'), hash, "account"]) == [submitter]
+    assert s.send(t.k0, c, 0, [z('get'), hash, "timestamp"]) == [s.block.timestamp]
+
+    # Creator kills the contract
+    assert s.send(t.k0, c, 0, [z('kill')]) == []
+    assert s.send(t.k0, c, 0, [z('get'), 0, 0]) == []
